430e3dc4b5ecb64a8c8f8450bc2d6d90cc2efb7f
yourleige@gmail.com <Fang201314>
2019-04-28 16:24:52 +0800

    修增加spring-session相关支持和新建spring-oath2相关的app项目

---
+++ leige-security-app-demo/src/main/java/com/leige/security/leigesecurityappdemo/LeigeSecurityAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.leige.security.leigesecurityappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeigeSecurityAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeigeSecurityAppDemoApplication.class, args);
+    }
+
+}
