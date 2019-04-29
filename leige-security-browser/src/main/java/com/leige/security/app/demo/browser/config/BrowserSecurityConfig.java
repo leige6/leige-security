31d24f7085911b5e6bb815b259497c43645dc6a8
yourleige@gmail.com <Fang201314>
2019-04-29 14:00:19 +0800

    Revert "修增加spring-session相关支持和新建spring-oath2相关的app项目"
    This reverts commit 430e3dc4

--- leige-security-browser/src/main/java/com/leige/security/app/demo/browser/config/BrowserSecurityConfig.java
+++ leige-security-browser/src/main/java/com/leige/security/app/demo/browser/config/BrowserSecurityConfig.java
@@ -5,7 +5,6 @@ import com.leige.security.browser.MyPasswordEncoder;
 import com.leige.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
 import com.leige.security.core.properties.SecurityConstants;
 import com.leige.security.core.properties.SecurityProperties;
-import com.leige.security.core.properties.SessionProperties;
 import com.leige.security.core.validate.code.ValidateCodeSecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -17,11 +16,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
-import org.springframework.security.web.session.InvalidSessionStrategy;
-import org.springframework.security.web.session.SessionInformationExpiredStrategy;
 
 import javax.sql.DataSource;
 
@@ -59,20 +55,6 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfigs;
 
-    /**
-     * @see BrowserSecurityBeanConfig
-     */
-    @Autowired
-    private InvalidSessionStrategy invalidSessionStrategy;
-    /**
-     * @see BrowserSecurityBeanConfig
-     */
-    @Autowired
-    private SessionInformationExpiredStrategy sessionInformationExpiredStrategy;
-
-    @Autowired
-    private LogoutSuccessHandler logoutSuccessHandler;
-
     @Bean
     public PersistentTokenRepository persistentTokenRepository() {
         // org.springframework.security.config.annotation.web.configurers.RememberMeConfigurer.tokenRepository
@@ -87,7 +69,6 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        SessionProperties session = securityProperties.getBrowser().getSession();
         //http.httpBasic()
         http.apply(validateCodeSecurityConfig) .and()
                 .apply(smsCodeAuthenticationSecurityConfigs)
@@ -101,26 +82,11 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
                 .tokenRepository(persistentTokenRepository())
                 .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
                 .userDetailsService(userDetailsService)
-                .and()
-                .sessionManagement()
-                .invalidSessionStrategy(invalidSessionStrategy)
-                .maximumSessions(session.getMaximumSessions()) //限制同一个用户只能有一个session登录
-                .maxSessionsPreventsLogin(session.isMaxSessionsPreventsLogin())// 当session达到最大后，阻止后登录的行为
-                .expiredSessionStrategy(sessionInformationExpiredStrategy)  // 失效后的策略。定制型更高，失效前的请求还能拿到
-                .and()
-                .and()
-                .logout()
-//                .logoutUrl("/singout")  // 退出请求路径
-                // 与logoutSuccessUrl互斥，有handler则logoutSuccessUrl失效
-                // 通过处理器增加配置了页面则跳转到页面，没有则输出json
-                .logoutSuccessHandler(logoutSuccessHandler)
-                .deleteCookies("JSESSIONID")
-                .and()
-                .authorizeRequests()
+       .and().authorizeRequests()
                 .antMatchers(SecurityConstants.DEFAULT_SESSION_INVALID_URL+".html",
                         SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
                         SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*",
-                        securityProperties.getBrowser().getLoginPage(),securityProperties.getBrowser().getSignOutUrl()).permitAll()
+                        securityProperties.getBrowser().getLoginPage()).permitAll()
        .anyRequest()
        .authenticated()
        .and()
